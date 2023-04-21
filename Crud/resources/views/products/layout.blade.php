af2db1b68eeabed37e0051d88c8ec09dd82daed3
BOBTHEBUILDERR <38834484+BOBTHEBUILDERR@users.noreply.github.com>
2023-04-21 07:17:44 +0000

    push

---
+++ Crud/resources/views/products/layout.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Mahesh Crud Application</title>
+  </head>
+  <body>
+    <div class="container">
+    	@yield('content')
+    </div>
+  </body>
+</html>
+</pre>
+<strong class="text-danger">resources/views/products/show.blade.php</strong>
+<pre class="prettyprint">
+@extends('products.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> Show Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {{ $product->name }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Details:</strong>
+            {{ $product->detail }}
+        </div>
+    </div>
+</div>
+@endsection
